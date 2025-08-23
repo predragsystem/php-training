b5f43105774a8e84701dc33f21b9fc72ae16a6a7
shirdemahesh <shirdemahesh18@gmail.com>
2025-08-23 10:43:07 +0530

    update query

---
+++ curd/index.php
@@ -0,0 +1,18 @@
+<?php 
+include('connection.php');
+
+$sqlData = "SELECT * FROM user";
+$result = mysqli_query($connection,$sqlData);
+$html = "<table border='1'>";
+$html .= "<tr><th>Name</th><th>Email</th></tr>";
+if(mysqli_num_rows($result)> 0){
+    while($row =($result)){
+        $html .= "<tr>";
+         $html .= "<td>".$row['name']."</td>";
+         $html .= "<td>".$row['email']."</td>";
+         $html .= "<td><a href=edit.php?id=".$row['id'].">Edit</a></td>";
+         
+        $html .= "</tr>"; 
+    }
+    echo $html;
+}
--- php/curd/update.php
+++ php/curd/update.php
@@ -1,2 +1,16 @@
 <?php 
-print_r($_POST);
+include('connection.php');
+if(isset($_POST['id'])){
+    $userid = $_POST['id'];
+    $name = $_POST['firstName'];
+    $email = $_POST['email'];
+
+    $sqlUpdate ="UPDATE user SET name= '$name' , email='$email' 
+                WHERE id = $userid";
+     if(mysqli_query($connection, $sqlUpdate)){
+        echo "User Updated Successfully";
+        header("Location:index.php");
+    } else {
+        echo "Something wrong".mysqli_error($connection);
+    }
+}
